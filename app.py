2064911c80e3b38b1264c50605be49fc936687dd
Sanaya James <sanaya.j.james@gmail.com>
2026-03-11 20:34:22 -0700

    Completed lab. Implemented home route, get_products, and get_product_by_id.

--- app.py
+++ app.py
@@ -7,19 +7,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+	# TODO: Return a welcome message
+
+	return jsonify({"message": "Welcome to the product page API!"}), 200
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+	# TODO: Return all products or filter by ?category=
+
+	category = request.args.get("category")
+	if category:
+		filtered = [item for item in products if item["category"] == category]
+		return jsonify(filtered), 200
+	# return jsonify({"message": "Products not found"}), 400
+	return jsonify(products), 200
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
-@app.route("/products/<int:id>")
+@app.route("/products/<int:id>", methods=["GET"])
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+	# TODO: Return product by ID or 404
+
+	for p in products:
+		if p["id"] == id:
+			return jsonify(p), 200
+	return jsonify({"message": "Product not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
